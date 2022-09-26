e538c87d883469e250a7944bef7bdf5d7053a504
nghiapham <npham2133@gmail.com>
2022-09-26 09:54:30 +0700

    Reload file

--- index.js
+++ index.js
@@ -3,4 +3,21 @@ $(document).ready(function() {
         $('#scroll').toggle()
         $('#scroll_1').toggle()
     });
+
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
 });
